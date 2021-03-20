448f019c815c272576ce2ad27c02158832586036
Abraham Serrano <abrahamserrano@MacBook-Pro-de-Abraham-2.local>
2021-03-20 14:04:21 -0600

    add reach router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Router } from "@reach/router";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import Home from "./pages/Home";
 export default function App() {
   return (
     <>
       <GlobalStyles />
-      <Home />
+      <Router>
+        <Home path="/" />
+      </Router>
     </>
   );
 }
